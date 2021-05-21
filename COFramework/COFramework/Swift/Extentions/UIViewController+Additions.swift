588527f0497f09c10b66a4b4a36f68ea58de7f97
Cenker Ozkurt <cenker.x.ozkurt@kp.org>
2021-05-21 12:44:05 -0700

    duplicate methods removed.

--- COFramework/COFramework/Swift/Extentions/UIViewController+Additions.swift
+++ COFramework/COFramework/Swift/Extentions/UIViewController+Additions.swift
@@ -9,35 +9,6 @@
 import UIKit
 
 extension UIViewController {
-
-    func hideKeyboardOnTap() {
-        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.closeKeyboard))
-        tap.cancelsTouchesInView = false
-        view.addGestureRecognizer(tap)
-    }
-
-    @objc func closeKeyboard() {
-        view.endEditing(true)
-    }
-    
-    func registerForKeyboardWillShowNotification(_ scrollView: UIScrollView) {
-        NotificationsCenterManager.sharedInstance.addObserver(self, forName: UIResponder.keyboardWillShowNotification.rawValue) { (notification) in
-            let userInfo = notification.userInfo!
-            let keyboardSize = (userInfo[UIResponder.keyboardFrameEndUserInfoKey]! as AnyObject).cgRectValue.size
-            let contentInsets = UIEdgeInsets(top: scrollView.contentInset.top, left: scrollView.contentInset.left, bottom: keyboardSize.height, right: scrollView.contentInset.right)
-
-            scrollView.contentInset = contentInsets
-            scrollView.scrollIndicatorInsets = contentInsets
-        }
-    }
-
-    func registerForKeyboardWillHideNotification(_ scrollView: UIScrollView) {
-        NotificationsCenterManager.sharedInstance.addObserver(self, forName: UIResponder.keyboardWillHideNotification.rawValue) { (notification) in
-
-            scrollView.contentInset = UIEdgeInsets.zero
-            scrollView.scrollIndicatorInsets = UIEdgeInsets.zero
-        }
-    }
     
     var isModal: Bool {
         if let index = navigationController?.viewControllers.firstIndex(of: self), index > 0 {
