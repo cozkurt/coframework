53520bb9ad6c73c519392b72a7e0a13308ace6f3
Cenker Ozkurt <cenker.x.ozkurt@kp.org>
2021-05-21 10:56:37 -0700

    source path updated

--- Package.swift
+++ Package.swift
@@ -5,5 +5,5 @@ import PackageDescription
 let package = Package(name: "COFramework",
                       platforms: [.iOS(.v10)],
                       products: [.library(name: "COFramework", targets: ["COFramework"])],
-                      targets: [.target(name: "COFramework", path: "Sources")],
+                      targets: [.target(name: "COFramework", path: "COFramework")],
                       swiftLanguageVersions: [.v5])
