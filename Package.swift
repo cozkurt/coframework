ff5c2b0b291ac02174862c538cbe82101ead6184
Cenker Ozkurt <cenker@yahoo.com>
2021-06-23 23:13:00 -0700

    /swift

--- Package.swift
+++ Package.swift
@@ -5,5 +5,5 @@ import PackageDescription
 let package = Package(name: "COFramework",
                       platforms: [.macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)],
                       products: [.library(name: "COFramework", targets: ["COFramework"])],
-                      targets: [.target(name: "COFramework", path: "COFramework/COFramework")]
+                      targets: [.target(name: "COFramework", path: "COFramework/COFramework/Swift")]
 )
