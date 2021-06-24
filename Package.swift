147fddec914eedb0cf482b9b124f244b228c6d15
Cenker Ozkurt <cenker@yahoo.com>
2021-06-23 23:05:57 -0700

    package updated

--- Package.swift
+++ Package.swift
@@ -2,17 +2,15 @@
 // swift-tools-version:5.0
 import PackageDescription
 
-let package = Package(
-    name: "COFramework",
-    products: [
-        .library(
-            name: "COFramework",
-            targets: ["COFramework"]),
-    ],
-    dependencies: [],
-    targets: [
-        .target(
-            name: "COFramework",
-            dependencies: []))
-    ]
-)
+let package = Package(name: "COFramework",
+                      platforms: [.macOS(.v10_12),
+                                  .iOS(.v10),
+                                  .tvOS(.v10),
+                                  .watchOS(.v3)],
+                      products: [.library(name: "COFramework",
+                                          targets: ["COFramework"])],
+                      targets: [.target(name: "COFramework",
+                                        path: "COFramework/COFramework",
+                                        dependencies: ["ObjectMapper"]
+                                        ]),
+                      swiftLanguageVersions: [.v5])
