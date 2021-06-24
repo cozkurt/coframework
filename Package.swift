7fc5427c5733fd0596f6da4a4d1a1ea876f352d6
Cenker Ozkurt <cenker@yahoo.com>
2021-06-23 23:18:46 -0700

    more package update

--- Package.swift
+++ Package.swift
@@ -4,6 +4,20 @@ import PackageDescription
 
 let package = Package(name: "COFramework",
                       platforms: [.macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)],
+                      
                       products: [.library(name: "COFramework", targets: ["COFramework"])],
-                      targets: [.target(name: "COFramework", path: "COFramework/COFramework/Swift")]
+                      
+                      dependencies: [
+                        // Dependencies declare other packages that this package depends on.
+                        .package(url: "https://github.com/tristanhimmelman/ObjectMapper", .upToNextMajor(from: "4.2.0"))
+                      ],
+                      
+                      targets: [.target(name: "COFramework",
+                                        dependencies: [
+                                          .product(name: "ObjectMapper", package: "ObjectMapper")
+                                        ],
+                                        path: "COFramework/COFramework/Swift")],
+                      
+                      swiftLanguageVersions: [.v5]
 )
+
