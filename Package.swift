7b16023013fc68a7df7ce3e586fe7333264bb4b6
Cenker Ozkurt <cenker@yahoo.com>
2021-06-23 23:09:44 -0700

    format fix

--- Package.swift
+++ Package.swift
@@ -3,14 +3,6 @@
 import PackageDescription
 
 let package = Package(name: "COFramework",
-                      platforms: [.macOS(.v10_12),
-                                  .iOS(.v10),
-                                  .tvOS(.v10),
-                                  .watchOS(.v3)],
-                      products: [.library(name: "COFramework",
-                                          targets: ["COFramework"])],
-                      targets: [.target(name: "COFramework",
-                                        path: "COFramework/COFramework",
-                                        dependencies: ["ObjectMapper"]
-                                        ]),
-                      swiftLanguageVersions: [.v5])
+                      platforms: [.macOS(.v10_12), .iOS(.v10), .tvOS(.v10), .watchOS(.v3)],
+                      products: [.library(name: "COFramework", targets: ["COFramework"])],
+                      targets: [.target(name: "COFramework", path: "COFramework/COFramework", dependencies: ["ObjectMapper"]) ])
