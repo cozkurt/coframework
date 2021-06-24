81c374458362e1865b9a179ad3331cefec3ef20d
Cenker Ozkurt <cenker@yahoo.com>
2021-06-24 00:17:04 -0700

    v12

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 
 let package = Package(name: "COFramework",
                       
-                      platforms: [.iOS(.v10)],
+                      platforms: [.iOS(.v12)],
 
                       products: [.library(name: "COFramework", targets: ["COFramework"])],
                       
