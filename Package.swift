29ce5563570fb4dbcd067b35a7c8d2085a636d21
Cenker Ozkurt <cenker@yahoo.com>
2021-06-23 23:00:36 -0700

    pcakage updated

--- Package.swift
+++ Package.swift
@@ -2,8 +2,17 @@
 // swift-tools-version:5.0
 import PackageDescription
 
-let package = Package(name: "COFramework",
-                      platforms: [.iOS(.v10)],
-                      products: [.library(name: "COFramework", targets: ["COFramework"])],
-                      targets: [.target(name: "COFramework", path: "COFramework/Swift/**.*")],
-                      swiftLanguageVersions: [.v5])
+let package = Package(
+    name: "COFramework",
+    products: [
+        .library(
+            name: "COFramework",
+            targets: ["COFramework"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "COFramework",
+            dependencies: []))
+    ]
+)
