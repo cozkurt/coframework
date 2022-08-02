e46bb5f955c4eda91c45f8274e90e3846fd844c4
Cenker Ozkurt <cenkeroz@amazon.com>
2022-08-01 22:19:25 -0700

    Sample folder added

---
+++ COFramework/Sample/StylesViewController.swift
@@ -0,0 +1,31 @@
+//
+//  StylesViewController.swift
+//  Plume
+//
+//  Created by Cenker Ozkurt on 7/16/18.
+//  Copyright © 2018 Plume Design, Inc. All rights reserved.
+//
+
+import UIKit
+
+
+class StylesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        AppearanceController.sharedInstance.loadAppearance("DemoStyle1", view: self.view)
+    }
+    
+    @IBAction func loadStyle1() {
+        AppearanceController.sharedInstance.loadAppearance("DemoStyle1", view: self.view)
+    }
+    
+    @IBAction func loadStyle2() {
+        AppearanceController.sharedInstance.loadAppearance("DemoStyle2", view: self.view)
+    }
+    
+    @IBAction func close() {
+        NotificationsController.sharedInstance.post("DISMISS")
+    }
+}
