30faa48b9a47f42a3cebbc2f0ba85f09a9263381
Cenker Ozkurt <cenker@yahoo.com>
2024-11-02 21:49:50 -0700

    package.swift file updated 1.5.1

--- Package.swift
+++ Package.swift
@@ -1,18 +1,27 @@
-
 // swift-tools-version:5.1
 import PackageDescription
 
-let package = Package(name: "COFramework",
-                      platforms: [.iOS(.v13)],
-
-                      products: [
-                        .library(name: "COFramework",
-                                 targets: ["COFramework"])],
-                      
-                      targets: [
-                        .target(name: "COFramework",
-                                dependencies: [],
-                                path: "COFramework")
-                      ],
-                      swiftLanguageVersions: [.v5]
+let package = Package(
+    name: "COFramework",
+    platforms: [.iOS(.v16)],
+    products: [
+        .library(
+            name: "COFramework",
+            targets: ["COFramework"]
+        )
+    ],
+    targets: [
+        // Target for Objective-C files
+        .target(
+            name: "COFrameworkObjC",
+            path: "COFramework/ObjC",
+            publicHeadersPath: "."
+        ),
+        // Target for Swift files that depends on the Objective-C target
+        .target(
+            name: "COFramework",
+            dependencies: ["COFrameworkObjC"],
+            path: "COFramework/Swift"
+        )
+    ]
 )
